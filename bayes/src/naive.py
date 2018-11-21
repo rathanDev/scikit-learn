cb976558021787afa311fe3c0c43588f84c127ca
jana <janarthan.paramanandam@ge.com>
2018-11-21 11:08:01 +0530

    Predict the class using gaussian naive bayes

--- bayes/src/naive.py
+++ bayes/src/naive.py
@@ -1,8 +1,11 @@
 import numpy as np
-X = np.array([[-1,1], [-2,-1], [-3,-2], [1,1], [2,1], [3,2]])
-Y = np.array([1, 1, 1, 2, 2, 2])
-
 from sklearn.naive_bayes import GaussianNB
+
+# training data - features
+X = np.array([[-1, 1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+
+# training data - labels
+Y = np.array([1, 1, 1, 2, 2, 2])
 
 # clf - classifier
 
@@ -12,4 +15,3 @@ p = clf.predict([[-0.8, -1]])
 
 print(p)
 # outputs the class it belongs to
-
